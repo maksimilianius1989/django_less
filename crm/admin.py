2caa0fc823d15850a3cc725e005c446fef020440
Maks <it.vimax@gmail.com>
2021-04-21 05:39:00 +0300

    Added comment

--- crm/admin.py
+++ crm/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Orders
+from .models import Orders, StatusCrm, Comments
 
 # Register your models here.
 admin.site.register(Orders)
+admin.site.register(StatusCrm)
+admin.site.register(Comments)
--- crm/views.py
+++ crm/views.py
@@ -7,19 +7,19 @@ from price.models import PriceCard, PriceTable
 
 # Create your views here.
 def first_page(request):
-    object_list = Orders.objects.all()
+    slider_list = CmsSlider.objects.all()
     pc_1 = PriceCard.objects.get(pk=1)
     pc_2 = PriceCard.objects.get(pk=2)
     pc_3 = PriceCard.objects.get(pk=3)
     price_table = PriceTable.objects.all()
     form = OrderForm()
-    slider_list = CmsSlider.objects.all()
     dict_obj = {
         'slider_list': slider_list,
         'pc_1': pc_1,
         'pc_2': pc_2,
         'pc_3': pc_3,
         'price_table': price_table,
+        'form': form,
     }
 
     return render(request, './index.html', dict_obj)
